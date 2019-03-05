1177122d1abdd0164c4edf2b916d9279d19709c2
Bryon <bryon.pectol@gmail.com>
2019-03-05 10:07:55 -0700

    basic person grid laid out

--- scripts/swpeople.js
+++ scripts/swpeople.js
@@ -1,6 +1,27 @@
-import { people } from '../data/people.js'
+import { people } from '../data/people.js';
 
 
 const men = people.filter(person => person.gender === 'male');
+const women = people.filter(person => person.gender === 'female');
+const droids = people.filter(person => person.gender === 'n/a');
+const other = people.filter(person => (person.gender === 'n/a') || 
+(person.gender === 'Hermaphrodite') || (person.gender === 'none'));
 
-console.log(men);
+
+console.log(men, women, other);
+
+
+const mainContainer = document.createElement('div');
+
+mainContainer.className = 'container';
+
+
+men.forEach((man) => {
+let manElement = document.createElement('div');
+manElement.className = 'box';
+manElement.textContent = man.name;
+mainContainer.appendChild(manElement)
+})
+
+
+document.body.appendChild(mainContainer)
