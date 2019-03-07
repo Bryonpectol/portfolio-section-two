f2a28b91754e5113b8929a44069ef74d0d8a5fc1
Bryon <bryon.pectol@gmail.com>
2019-03-07 12:48:03 -0700

    loaded images and did more things on js

--- scripts/swpeople.js
+++ scripts/swpeople.js
@@ -1,35 +1,15 @@
 import { people } from '../data/people.js';
 import { planets } from '../data/planets.js';
 
-const men = people.filter(person => person.gender === 'male');
-const women = people.filter(person => person.gender === 'female');
-const other = people.filter(person => (person.gender === 'n/a') || 
-(person.gender === 'Hermaphrodite') || (person.gender === 'none'));
 
 
 
 
 
-const allHomeWorlds = people.map(person => {
-    let foundWorld = planets.find(element => {
-      return  element.url === person.homeworld
-    })
-    return {name: person.name, home:  person.homeworld}
-});
 
-console.log(allHomeWorlds);
 
-// const peopleAndPlanets = [...people, ...planets]
 
-// const homeWorld = peopleAndPlanets.find(element => {
-//     console.log(element.url, element.homeworld)
-// })
 
-// console.log(peopleAndPlanets);
-
-const mainContainer = document.createElement('div');
-
-mainContainer.className = 'container';
 
 
 men.forEach((man) => {
@@ -41,26 +21,5 @@ eyeColor.textContent = man.eye_color;
 manElement.appendChild(eyeColor);
 mainContainer.appendChild(manElement);
 });
-
-women.forEach((man) => {
-    let manElement = document.createElement('div');
-    manElement.className = 'box';
-    manElement.textContent = man.name;
-    let eyeColor = document.createElement('p');
-    eyeColor.textContent = man.eye_color;
-    manElement.appendChild(eyeColor);
-    mainContainer.appendChild(manElement);
-    });
-
-    other.forEach((man) => {
-        let manElement = document.createElement('div');
-        manElement.className = 'box';
-        manElement.textContent = man.name;
-        let eyeColor = document.createElement('p');
-        eyeColor.textContent = man.eye_color;
-        manElement.appendChild(eyeColor);
-        mainContainer.appendChild(manElement);
-        });
-
 
 document.body.appendChild(mainContainer);
