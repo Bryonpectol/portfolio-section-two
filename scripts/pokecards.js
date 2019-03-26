febcd8288add09f953eb8bebd9033d41600d7f92
Bryon <bryon.pectol@gmail.com>
2019-03-26 12:46:04 -0600

    added pokemon cards page

---
+++ scripts/pokecards.js
@@ -0,0 +1,32 @@
+import { pokemon } from '../data/pokemon.js';
+
+console.log(pokemon);
+
+const mainContainer = document.querySelector('.container')
+
+  function createpokecard(pokedata) {
+      let card = document.createElement('div')
+      card.className = 'box'
+      let title = document.createElement('h2')
+      let image = document.createElement('img')
+
+      let UpperName = pokedata.name.charAt(0).toUpperCase() + pokedata.name.slice(1)
+      title.textContent = UpperName
+      image.src = pokedata.sprites.front_shiny
+      card.appendChild(title)
+      card.appendChild(image)
+    mainContainer.appendChild(card)
+  }
+
+  pokemon.forEach((singleMon) => {
+    fetch(singleMon.url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      createpokecard(myJson)
+    });
+  
+  })
+
+
