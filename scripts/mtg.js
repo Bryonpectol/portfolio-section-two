27338862d2a8fe3cc7f4ae6425676a44a55961ac
Bryon <bryon.pectol@gmail.com>
2019-03-27 19:00:03 -0600

    i give up

--- scripts/mtg.js
+++ scripts/mtg.js
@@ -6,7 +6,6 @@ const allCards = mtgdata.map(myCard => {
     let cardsYeah = mtgdata.find(CARD => {
       return CARD.name === myCard.name;
     });
-
     return {
       // this is how the script finds the item in the array.
       nAME: myCard.name,
@@ -46,56 +45,5 @@ const allCards = mtgdata.map(myCard => {
 
       // Appended Childeren, the order the elements appear.
       mainContainer.appendChild(cardsElement);
-      cardsElement.appendChild(cardElement);
-    //   makeElement.appendChild(originElement);
       cardsElement.appendChild(imageElement);
 
-    //   makeElement.appendChild(br11Element);
-      // makeElement.appendChild(cylindersElement);
-
-    //   makeElement.appendChild(br1Element);
-     
-    //   makeElement.appendChild(yearElement);
-
-    //   makeElement.appendChild(br2Element);
-    //   makeElement.appendChild(priceElement);
-
-    //   makeElement.appendChild(br3Element);
-    //   makeElement.appendChild(engineElement);
-
-    //   makeElement.appendChild(br4Element);
-    //   makeElement.appendChild(hpElement);
-
-    //   makeElement.appendChild(br5Element);
-    //   makeElement.appendChild(driveElement);
-
-    //   makeElement.appendChild(br6Element);
-    //   makeElement.appendChild(transElement);
-
-    //   makeElement.appendChild(br7Element);
-    //   makeElement.appendChild(weightElement);
-      
-    //   makeElement.appendChild(br12Element);
-    //   makeElement.appendChild(mphkphElement);
-
-    //   makeElement.appendChild(br8Element);
-    //   makeElement.appendChild(zeroto60Element);
-
-    //   makeElement.appendChild(br9Element);
-    //   makeElement.appendChild(mpgElement);
-
-    //   makeElement.appendChild(br10Element);
-    //   makeElement.appendChild(torqueElement);
-      
-    //   makeElement.appendChild(br11Element);
-    //   makeElement.appendChild(cylindersElement);
-
-     
-  });
-
-  // You need this.
-  document.body.appendChild(mainContainer);
-  document.body.appendChild(footerElement);
-
-  console.log(allCards);
-  console.log(mtgdata)
