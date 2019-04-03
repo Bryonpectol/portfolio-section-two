c3f4a355d80ef3c8df1e117de344ace89bcd6e7f
Bryon <bryon.pectol@gmail.com>
2019-04-02 23:52:41 -0600

    added ability buttons again

--- scripts/hotscards.js
+++ scripts/hotscards.js
@@ -14,31 +14,49 @@ const allCars = heros.map(myCar => {
       mAKE: carsYeah.name,
       role: carsYeah.role,
       image: carsYeah.image,
-      type: carsYeah.type
-   
+      type: carsYeah.type,
+      ability1: carsYeah.ability1,
+      ability2: carsYeah.ability2,
     };
   });
 
   allCars.forEach(myCar => {
 
+    //br's
+    let br3 = document.createElement('br');
+    let br1 = document.createElement('br');
+    let br2 = document.createElement('hr');
 
     // this section is to create the html elements.
     let makeElement = document.createElement('div');
     let roleElement = document.createElement('p');
     let imageElement = document.createElement('img');
-    let br1 = document.createElement('br');
     let typeElement = document.createElement('p');
-    let br2 = document.createElement('hr');
+    let ability1Element = document.createElement('p');
+    let ability1button = document.createElement('button');
+    let ability2Element = document.createElement('p');
+    let ability2button = document.createElement('button');
 
 
   // assigning classes.
   makeElement.className = 'box';
   imageElement.className = 'imagecard';
+  roleElement.className = 'role';
+  typeElement.className = 'role';
+  ability1Element.className = 'roles';
+  ability1button.className = 'ability1button';
+  ability2Element.className = 'roles';
+  ability2button.className = 'ability2button';
+
+  ability1button.innerHTML = "Ability 1";
+  ability2button.innerHTML = "Ability 2";
 
     makeElement.textContent = myCar.mAKE;
     roleElement.textContent = myCar.role;
     imageElement.src = myCar.image;
     typeElement.textContent = myCar.type;
+    ability1Element.textContent = myCar.ability1;
+    ability2Element.textContent = myCar.ability2;
 
 
     
@@ -48,7 +66,23 @@ const allCars = heros.map(myCar => {
     makeElement.appendChild(roleElement);
     makeElement.appendChild(br1);
     makeElement.appendChild(imageElement);
+    makeElement.appendChild(br3);
+    makeElement.appendChild(ability1button);
+    makeElement.appendChild(ability2button);
     
+    ability1button.onclick = function() {Ability1()};
+
+    function Ability1() {
+      makeElement.appendChild(ability1Element);
+    }
+
+    ability2button.onclick = function() {Ability2()};
+
+    function Ability2() {
+      makeElement.appendChild(ability2Element);
+    }
+    
+});
 
 document.body.appendChild(mainContainer);
 
