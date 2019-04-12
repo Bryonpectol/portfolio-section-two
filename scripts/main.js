d4473c6fc39464b2731f6060fe582a8b6162c154
Bryon <bryon.pectol@gmail.com>
2019-04-11 23:27:54 -0600

    added burger menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,9 @@
-
-
-films.sort((a,b) => (a.episode_id > b.episode_id) ? 1 : -1);
-
-// const result = films.filter(film => film.)
-
-const intro = document.querySelector('.intro')
-
-films.forEach((films) => {
-
-    let tile = document.createElement('div');
-
-    let titleElement = document.createElement('h1')
-    let crawlElement = document.createElement('h3')
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
