268e64722bd308c2290b4d979fbcbbae4a10dd3c
Bryon <bryon.pectol@gmail.com>
2019-10-11 21:28:03 -0600

    added contact form

--- scripts/contactyes.php
+++ scripts/contactyes.php
@@ -1,3 +1,17 @@
 <?php
+if (isset($_POST['submit'])) {
+  $first_name = $_POST['first_name'];
+  $last_name = $_POST['last_name'];
+  $mailFrom = $_POST['email'];
+  $telephone = $_POST['telephone'];
+  $message = $_POST['comments'];
 
+  $mailTo = "bryon@pectol.com";
+  $headers = "Bryon's Portfolio Contact Form: ".$mailFrom;
+  $txt = "This email is from: ".$first_name." ".$last_name.".\n\n".$message;
+
+  mail($mailTo, $mailFrom, $txt, $headers);
+  header("Location: contact.html?mailsend");
+
+}
 ?>
